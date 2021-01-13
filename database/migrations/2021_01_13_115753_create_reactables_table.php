24fcde7aa2f9fdd00bc10ce2860de3357cb93f14
masud <masudpado@gmail.com>
2021-01-13 19:59:00 +0600

    craete database schema

---
+++ database/migrations/2021_01_13_115753_create_reactables_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReactablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reactables', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('reaction_id')
+                ->constrained()
+                ->onDelete('cascade');
+            $table->morphs('reactable');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reactables');
+    }
+}
